55e4e4d980aee1c7d4f79c73b7d4700267cc800c
Muhammed Anas <muhammadanaswayanad@gmail.com>
2017-12-23 07:58:41 +0530

    :)

---
+++ weather/js/index.js
@@ -0,0 +1,34 @@
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: "Wayanad, KL",
+    woeid: "",
+    unit: "c",
+    success: function(weather) {
+      html =
+        '<h2><i class="icon-' +
+        weather.code +
+        '"></i> ' +
+        weather.temp +
+        "&deg;" +
+        weather.units.temp +
+        "</h2>";
+      html += "<ul><li>" + weather.city + ", " + weather.region + "</li>";
+      html += '<li class="currently">' + weather.currently + "</li>";
+      html +=
+        "<li>" +
+        weather.wind.direction +
+        " " +
+        weather.wind.speed +
+        " " +
+        weather.units.speed +
+        "</li></ul>";
+
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html("<p>" + error + "</p>");
+    }
+  });
+});
